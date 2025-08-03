7f937d17a2f04b723966be25da897651aa6f84ed
regmysss <kabanigor3@gmail.com>
2025-08-03 21:44:57 +0300

    feat: add user registration functionality with Prisma and bcrypt
    - Updated package.json to include Prisma client, bcrypt, and related dependencies.
    - Created initial Prisma migration for users table with unique email constraint.
    - Added Prisma schema definition for User model.
    - Implemented user registration form with API integration.
    - Created API route for user registration, including email uniqueness check and password hashing.
    - Set up database connection with Prisma client and acceleration extension.

--- src/app/(auth)/register/RegisterForm.tsx
+++ src/app/(auth)/register/RegisterForm.tsx
@@ -3,10 +3,13 @@ import AuthFormInput from '@/components/ui/AuthFormInput'
 import GradientButton from '@/components/ui/GradientButton'
 import { REGISTER_CONFIG } from '@/shared/configs/AuthFormConfigs'
 import { PAGES } from '@/shared/constants/routes'
+import { useRouter } from 'next/navigation'
 import Link from 'next/link'
 import { useState } from 'react'
 
 export default function RegisterForm() {
+    const router = useRouter();
+
     const [formData, setFormData] = useState({
         name: '',
         email: '',
@@ -14,10 +17,33 @@ export default function RegisterForm() {
         confirmPassword: ''
     })
 
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const response = await fetch('/api/user', {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                name: formData.name,
+                email: formData.email,
+                password: formData.password
+            })
+        })
+
+        if (response.ok) {
+            router.push(PAGES.LOGIN);
+        }
+        else {
+            console.error('Failed to register user!');
+        }
+    }
+
     return (
         <form
             className="bg-white/50 p-3 rounded-4xl max-w-[500px] w-full"
-            action=""
+            onSubmit={handleSubmit}
         >
             <div className="bg-white w-full py-4 px-8 rounded-[20px] flex flex-col items-center">
                 <h1 className="text-xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#2754C8] to-[#110F72] mb-4">Register</h1>
@@ -28,7 +54,7 @@ export default function RegisterForm() {
                         return <AuthFormInput
                             key={input.name}
                             {...props}
-                            value={formData[input.name as keyof typeof formData]}
+                            value={formData[input.name as keyof typeof formData] ?? ''}
                             onChange={(e) => setFormData({ ...formData, [input.name]: e.target.value })}
                         />
                     })
